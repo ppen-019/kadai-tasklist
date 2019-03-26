4d6129891aeccd2bc7a974346560bac8d234da08
ppen.019 <ppengineer.019@gmail.com>
2019-03-26 16:07:55 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,8 +9,17 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="nav-item">{!! link_to_route('signup.get', 'Signup', null, ['class' => 'nav-link']) !!}</li>
-                <li class="nav-item">{!! link_to_route('#', 'Login', null, ['class' => 'nav-link']) !!}</li>
+                @if (Auth::check())
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', null, ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('login', 'Login', null, ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
